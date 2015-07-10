7742a697bfb38d130cdb63e8eb586727f3323b0f
mackenzie.marshall <mackenzie.marshall@hootsuite.com>
2015-07-10 16:20:14 -0700

    Added custom webview to disable navigation away from comments

---
+++ app/src/main/java/com/example/mackenziem/redditimageviewer/CustomWebViewClient.java
@@ -0,0 +1,12 @@
+package com.example.mackenziem.redditimageviewer;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class CustomWebViewClient extends WebViewClient {
+    private String setUrl;
+
+    public CustomWebViewClient(String url) {
+        setUrl = url;
+    }
+}
